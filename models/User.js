f2dafafeca27b2f9d3749cb20f11b9abba6743e1
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-05-11 10:48:58 +0300

    password verification

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, Datatypes } = require('sequelize')
 const sequelize = require('../config/connection')
 const bcrypt = require('bcrypt')
 
-class User extends Model {}
+class User extends Model {
+  //set up method to run on instance data (per user) to check password
+  checkPassword(loginPW){
+    return bcrypt.compareSync(loginPw, this.password)
+  }
+}
 
 //define table columns and configuration
 User.init(
